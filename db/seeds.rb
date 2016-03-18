36144966ed1e36040fc729f4c395d97e3921f02a
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-03-18 14:24:11 -0400

    add page styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ end
 
 100.times do |n|
 	airports = Airport.all.sample(2)
-	departs = time_rand(Time.now, Time.local(2016,04,01))
+	departs = time_rand(Time.now, Time.local(2016,03,25))
 	duration = rand(1.0..8.0).round(2)
 	puts "Creating flight #{airports[0].code} -> #{airports[1].code}, #{departs}, #{duration} hrs"
 	Flight.create!(to_airport_id: airports[0].id, from_airport_id: airports[1].id, departure: departs, duration: duration)
