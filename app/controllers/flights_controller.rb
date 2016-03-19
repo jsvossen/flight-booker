f7399880c267d666c4bb4d8a9b6f463475ea494a
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-03-19 13:31:23 -0400

    make date search optional

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,8 +2,9 @@ class FlightsController < ApplicationController
 
 	def index
 		@booking = Booking.new
-		if (params[:departure])
-			@flights = Flight.on_day(params[:departure]).where(from_airport_id: params[:from_airport], to_airport_id: params[:to_airport])
+		if (params[:from_airport] || params[:to_airport])
+			@flights = Flight.where(from_airport_id: params[:from_airport], to_airport_id: params[:to_airport])
+			@flights = @flights.on_day(params[:departure]) if params[:departure] && !params[:departure].empty?
 		end
 	end
 
